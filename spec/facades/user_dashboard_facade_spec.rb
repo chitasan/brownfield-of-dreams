0602e9f0c7cd6a7e87d856ad836e7819afbf32c9
JennicaStiehl <jennica.stiehl@gmail.com>
2019-05-12 14:20:13 -0600

    adds facade for the following method/card

--- spec/facades/user_dashboard_facade_spec.rb
+++ spec/facades/user_dashboard_facade_spec.rb
@@ -1,30 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe 'UserDashboardFacade' do 
-  it 'exists' do 
+RSpec.describe 'UserDashboardFacade' do
+  it 'exists' do
     user = create(:user)
     facade = UserDashboardFacade.new(user)
 
     expect(facade).to be_a(UserDashboardFacade)
-  end 
+  end
 
-  describe 'instance methods' do 
-    it '.header' do 
+  describe 'instance methods' do
+    it '.header' do
       VCR.use_cassette('services/get_repos') do
         github_user = create(:user, token: ENV["CHI_USER_TOKEN"])
         facade = UserDashboardFacade.new(github_user)
 
         expect(facade.header).to eq("5 Repos")
-      end 
-    end 
+      end
+    end
 
-    it '.repos_top_five' do 
+    it '.repos_top_five' do
       VCR.use_cassette('services/get_repos') do
         github_user = create(:user, token: ENV["CHI_USER_TOKEN"])
         facade = UserDashboardFacade.new(github_user)
 
         expect(facade.repos_top_five.count).to eq(5)
-      end 
-    end 
-  end 
-end 
+      end
+    end
+    it '.following' do
+      VCR.use_cassette('services/get_following') do
+        github_user = create(:user, token: ENV["CHI_USER_TOKEN"])
+        facade = UserDashboardFacade.new(github_user)
+
+         expect(facade.following.count).to eq(8)
+      end
+    end
+  end
+end
