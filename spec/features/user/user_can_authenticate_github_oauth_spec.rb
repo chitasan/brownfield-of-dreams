eabc2674c5ee4d55058dbbb4c3756d713660bb84
Chi Tran <chidtran@gmail.com>
2019-05-13 20:06:36 -0600

    Adds happy path testing for Github OAuth, plus cassettes

--- spec/features/user/user_can_authenticate_github_oauth_spec.rb
+++ spec/features/user/user_can_authenticate_github_oauth_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'A Registered User' do 
-  describe 'can connect to GitHub via OAuth' do 
-    it 'through a button on /dashboard', :vcr do 
+  describe 'with nil token' do 
+    it 'can authenticate via GitHub OAuth', :vcr do 
       OmniAuth.config.mock_auth[:github] = nil
       OmniAuth.config.test_mode = true
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
@@ -36,5 +36,24 @@ RSpec.describe 'A Registered User' do
         expect(page).to have_css('.repo-link', count: 5)
       end 
     end 
+  end 
+
+  describe 'with token' do 
+    it 'does not need to authenticate with Github OAuth', :vcr do 
+      github_user = create(:user, token: ENV["CHI_USER_TOKEN"])
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(github_user)
+
+      visit '/dashboard'
+      expect(current_path).to eq(dashboard_path)
+
+      expect(page).to_not have_button('Connect to Github')
+      expect(page).to have_content("GitHub")
+      expect(page).to have_content("5 Repos")
+      save_and_open_page
+      within '.github_repos' do
+        expect(page).to have_css('.repo-link')
+      end
+    end
   end 
 end 
