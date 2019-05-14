34ec2f2f7b78e253651e921af6fe12673071d25d
Chi Tran <chidtran@gmail.com>
2019-05-14 14:23:23 -0600

    Removes comments

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,7 +2,6 @@ class Admin::BaseController < ApplicationController
   before_action :require_admin
 
   def require_admin
-    # four_oh_four unless current_user.admin?
     render file: 'public/404', status: 404 unless current_admin?
   end
 end
