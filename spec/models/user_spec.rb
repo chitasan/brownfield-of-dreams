40e47dd7d201bf114926785c931089b35a76a558
Chi Tran <chidtran@gmail.com>
2019-05-15 22:13:20 -0600

    Adds show bookmarked tutorials instance method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,4 +22,33 @@ RSpec.describe User, type: :model do
       expect(admin.admin?).to be_truthy
     end
   end
+
+  describe 'instance methods' do
+    it '.show_bookmarked_tutorials' do
+      user = create(:user)
+
+      tutorial_1 = create(:tutorial, title: 'How to Tie Your Shoes')
+
+      video_1 = create(:video, title: 'The Bunny Ears Technique', tutorial: tutorial_1, position: 2)
+      video_2 = create(:video, title: "I don't know how to tie my shoes", tutorial: tutorial_1, position: 1)
+
+      create(:user_video, user: user, video: video_1)
+      create(:user_video, user: user, video: video_2)
+
+      bookmarked = {
+        {
+          tutorial_1.id => tutorial_1.title
+        } => [
+          {
+            video_2.id => video_2.title
+          },
+          {
+            video_1.id => video_1.title
+          }
+        ]
+      }
+
+      expect(user.show_bookmarked_tutorials).to eq(bookmarked)
+    end 
+  end 
 end
