0ad951b94c5d28019901292c343836bc08392f36
Chi Tran <chidtran@gmail.com>
2019-05-15 22:32:41 -0600

    Fixes test expected results for .followers

--- spec/facades/user_dashboard_facade_spec.rb
+++ spec/facades/user_dashboard_facade_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe 'UserDashboardFacade' do
         github_user = create(:user, token: ENV["CHI_USER_TOKEN"])
         facade = UserDashboardFacade.new(github_user)
         
-        expect(facade.followers.count).to eq(3)
+        expect(facade.followers.count).to eq(4)
       end 
     end 
   end 
