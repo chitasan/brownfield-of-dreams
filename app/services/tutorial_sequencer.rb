c4efbb7bf07fd270ba07bca8742abef253c9dd71
Chi Tran <chidtran@gmail.com>
2019-05-17 16:35:03 -0600

    Adds rubocop errors fix

--- app/services/tutorial_sequencer.rb
+++ app/services/tutorial_sequencer.rb
@@ -9,6 +9,7 @@ class TutorialSequencer
   end
 
   private
+
   attr_reader :tutorial, :sequenced_video_ids
 
   def videos
@@ -17,12 +18,12 @@ class TutorialSequencer
 
   def update_position_if_changed!
     sequenced_video_ids.each.with_index(1) do |video_id, index|
-      video = videos.find do |video|
+      video_found = videos.find do |video|
         video.id == video_id.to_i
       end
 
-      if video.position != index
-        video.update(position: index)
+      if video_found.position != index
+        video_found.update(position: index)
       end
     end
   end
